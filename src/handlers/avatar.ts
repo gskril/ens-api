5a6f3020803895348b610e4bdbf3fdae8e3e08ae
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-07-31 23:38:58 -0400

    ah

--- src/handlers/avatar.ts
+++ src/handlers/avatar.ts
@@ -46,6 +46,9 @@ export async function handleAvatar(request: IRequest, env: Env, ctx: ExecutionCo
     gatewayUrls: { ipfs: 'https://ipfs.punkscape.xyz' },
   });
 
+  const imgRes = await fetch(avatarUrl);
+  const contentType = imgRes.headers.get('content-type');
+
   console.log({ textRecord, avatarUrl });
 
   // Note: Cloudflare sanitizes SVGs by default so we don't need extra checks here
@@ -54,6 +57,7 @@ export async function handleAvatar(request: IRequest, env: Env, ctx: ExecutionCo
     headers: {
       ...request.headers,
       'X-API-KEY': avatarUrl.includes('.seadn.io') ? env.OPENSEA_API_KEY : '',
+      'Content-Type': contentType || 'image/svg+xml',
     },
     cf: {
       cacheTtl: 3600,
@@ -72,6 +76,7 @@ export async function handleAvatar(request: IRequest, env: Env, ctx: ExecutionCo
     return res;
   } else {
     console.log({ res: res.status, ok: res.ok });
+    console.log('failed res', res);
     return fallbackResponse(ctx, cache, cacheKey, fallback);
   }
 }
